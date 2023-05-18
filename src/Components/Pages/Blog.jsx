3140f0a5b1458dfd3160a599a42405e6b6b29a04
Sk Shariful Islam <skshariful10@gmail.com>
2023-05-18 20:53:49 +0600

    First Question Added With Answer On Blog Page

--- src/Components/Pages/Blog.jsx
+++ src/Components/Pages/Blog.jsx
@@ -10,12 +10,22 @@ const Blog = () => {
 				href="#"
 				class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-					What is an access token and refresh token? How do they work and where should we
-					store them on the client-side?
+					What is an access token and refresh token? How do they work and where should we store them on the client-side?
 				</h5>
 				<p class="font-normal text-gray-700 dark:text-gray-400">
-					Here are the biggest enterprise technology acquisitions of 2021 so far, in
-					reverse chronological order.
+					Access Token: An access token is a short-lived credential that proves a user or application's authentication and authorization to access 
+                    protected resources on an API or server.
+				</p>
+				<p class="font-normal text-gray-700 dark:text-gray-400">
+					Refresh Token: A refresh token is a long-lived credential used to obtain a
+					newaccess token without requiring the user to reauthenticate, enhancing security by minimizing the exposure of the access token.
+				</p>
+				<p class="font-normal text-gray-700 dark:text-gray-400">
+					Access tokens can be stored in memory or short-lived storage mechanisms 
+                    like sessions or cookies. Refresh tokens should be securely stored 
+                    in an HTTP-only cookie with secure and HTTPS-only settings. 
+                    Following security best practices is crucial to prevent 
+                    unauthorized access.
 				</p>
 			</div>
 			<div
